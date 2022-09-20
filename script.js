6bfa59dd1621e6cbd5780149c12bf5c3dbc6e224
Asgarov Asgar <asgara@maestro.st>
2022-09-20 23:32:44 +0400

    added destructuring objects and the spread operator

--- script.js
+++ script.js
@@ -16,6 +16,17 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
   },
 
+  orderDelivery: function({starterIndex, mainIndex, time, address}) {
+    console.log(starterIndex, mainIndex, time, address);
+    console.log(
+      `Order delivered. ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+  orderPasta: function(ing1, ing2, ing3 = 'iney') {
+    console.log(`Here is your delicious pasta: 
+    ${ing1}, ${ing2} and ${ing3}`);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -32,35 +43,97 @@ const restaurant = {
   },
 };
 
-//DESTRUCTURING
-const arr = [2, 3, 4]
+// SPREAD OPERATOR - taking all elements out of array
+// does NOT manipulate original array
 
-const [x, y, z] = arr
+// const arr = [7,8,9]
+// const newBadArr = [ 1, 2, arr[0], arr[1], arr[2] ]
+// const newArr = [1, 2, ...arr, ...[42, ...[8, 9], 12]]
+// console.log(newArr);
+// console.log(...newArr);
+
+// const newMenu = [...restaurant.mainMenu, 'Dolma'];
+// console.log(newMenu);
+
+// Copy Array - Shallow Copy
+// const mainMenuCopy = [...restaurant.mainMenu]
+
+// Join 2 array
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// Iterables: strings, arrays, maps, sets, NOT objects
+// from 2020 spread operator also work with objects, even they are not iterables
+
+// const str = 'Asgar'
+// const newStr = [...str, ' ', 'U']
+// console.log(newStr);
+
+// const ings = [prompt('Add 1st ing'), prompt('Add 2nd ing')];
+
+// restaurant.orderPasta(...ings)
+
+// const newRestaurant = {...restaurant, founder: 'Asgar 😀'}
+// console.log(newRestaurant);
+
+
+
+// DESTRUCTURING OBJECTS
+
+// restaurant.orderDelivery({
+//   time: '20:30',
+//   address: 'Baku, NN',
+//   mainIndex: 1,
+//   starterIndex: 2
+// })
+
+// const {name, categories, openingHours}  = restaurant
+// console.log(name, categories, openingHours);
+
+// const {  name: restaurantName, categories: tags, openingHours: hours} = restaurant
+// console.log(restaurantName, tags, hours);
+
+
+// MUTATING VARIABLES
+// let a = 111
+// let b = 999
+// const obj = { a: 12, b: 44, c: 32 }
+// // we need to add parentheses in order to make it to work properly, otherwise JS understand {} as a code block
+// ({ a, b } = obj)
+// console.log(a, b);
+
+// const { fri: { open, close } } = openingHours
+// console.log(open, close);
+
+
+
+//DESTRUCTURING ARRAYS
+// const arr = [2, 3, 4]
+
+// const [x, y, z] = arr
 
 // console.log(x,y,z);
 
-let [main, , secondary] = restaurant.categories
+// let [main, , secondary] = restaurant.categories
 
-console.log('Original: ', main, secondary);
+// console.log('Original: ', main, secondary);
 
 // const temp = main
 // main = secondary
 // secondary = temp
 
 // switching reassigment
-[secondary, main] = [main, secondary]
+// [secondary, main] = [main, secondary]
 
-console.log('Switching: ', main, secondary);
+// console.log('Switching: ', main, secondary);
 
-console.log(restaurant.order(2, 0));
+// console.log(restaurant.order(2, 0));
 
-const [starter, mainCourse] = restaurant.order(2, 0);
+// const [starter, mainCourse] = restaurant.order(2, 0);
 
-console.log(starter, mainCourse);
+// console.log(starter, mainCourse);
 
 // nested destructuring
-const nested = [2, 4, [5, 6]]
-const [i, , [j, k]] = nested
-console.log(i, j, k);
-
-objects
+// const nested = [2, 4, [5, 6]]
+// const [i, , [j, k]] = nested
+// console.log(i, j, k);
