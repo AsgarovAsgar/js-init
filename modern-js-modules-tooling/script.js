480f4a5d18a04623c6cc0b3883c39dbb37222718
Asgarov Asgar <asgara@maestro.st>
2023-03-27 00:16:17 +0400

    bundled with parcel

--- modern-js-modules-tooling/script.js
+++ modern-js-modules-tooling/script.js
@@ -3,3 +3,20 @@ console.log('Importing module');
 
 addToCart('bread', 5)
 addToCart('money', 12)
+
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 }
+  ],
+  user: { loggedIn: true }
+}
+
+const stateClone = Object.assign({}, state)
+const stateDeepClone = cloneDeep(state)
+state.user.loggedIn = false
+console.log('stateClone', stateClone);
+console.log('stateDeepClone', stateDeepClone);
